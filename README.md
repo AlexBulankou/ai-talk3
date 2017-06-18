627a91b3055d10cbd58db592f21117b2234c0bbe
Alex Bulankou <bulankou@gmail.com>
2017-06-18 11:38:34 -0700

    Adding load generator, monitoring, redis

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ docker build -f be.dockerfile -t bulankou/ai-talk3:be_0.3 .
 ``docker push bulankou/ai-talk3:be_0.3``
 
 
-## Run in minikube
+## Deploy with kubernetes minikube
 
 * Publish to minikube (first remove already existing service)
 ``
@@ -25,5 +25,9 @@ kubectl create -f k8.yaml
 * After waiting for exposure to propage you can get IP by running:
 ``minikube service --url ai-talk3-service``
 
-* Test it:
+* Test it (replace port with the port obtained in previous command): 
 ``curl -i http://192.168.99.100:30121/stock?s={aapl,msft,amzn,fb}``
+
+## Run with docker-compose
+* Deploy with docker-compose
+``docker compose -f docker-compose.yaml up``
--- be/src/app.js
+++ be/src/app.js
@@ -1,74 +1,100 @@
 'use strict';
 
 const express = require('express');
-const mongoClient = require('mongodb').MongoClient;
 const url = require('url');
 const http = require('http');
 const appInsights = require("applicationinsights");
+const redis = require('redis');
+
 const app = express();
 
 // Constants
 const PORT = 8080;
-const MONGO_PORT = 27017;
+const REDIS_PORT = 6379;
+const REDIS_HOST = "redis";
 
 appInsights.setup(process.env.AI_KEY).start();
+var appInsightsClient = appInsights.client;
+
+var redisClient = redis.createClient(REDIS_PORT, REDIS_HOST);
+redisClient.on('connect', function () {
+    appInsightsClient.trackEvent('RedisClientConnected');
+});
+
+var getFromFinanceServer = function (symbol, onResponse) {
+    symbol = symbol.toLowerCase();
+    var path = "/finance/info?client=ig&q=";
+    if (Math.random() > 0.7) {
+        path = "/finance/info?client=ig&c=";
+    }
+    http.get({ host: "finance.google.com", path: path + symbol }, function (response) {
+        if (response.statusCode !== 200) {
+            onResponse(null);
+        }
+
+        var stockData = "";
+        response.on('data', function (d) {
+            stockData += d;
+        });
+        response.on("end", function () {
+            onResponse(stockData);
+        });
+        response.on("error", function () {
+            onResponse(null);
+        });
+    });
+}
 
 app.get('/stock', function (req, res) {
     var url_parts = url.parse(req.url, true);
     var query = url_parts.query;
     var stock = query["s"];
+    var data = null;
+
     if (stock) {
+        // Normalize stock symbol
         stock = stock.toLowerCase();
-        var path = "/finance/info?client=ig&q=";
-        if (Math.random() > 0.7) {
-            path = "/finance/info?client=ig&c=";
-        }
 
-        http.get({ host: "finance.google.com", path: path + stock }, function (response) {
-            if (response.statusCode === 400) {
-                res.statusCode = 204;
-                res.end();
+        // Try retrieving from Redis
+        redisClient.get(stock, function (err, reply) {
+            // if it is Redis, use the value
+            if (reply) {
+                console.log(stock + " data found in redis");
+                data = reply;
+            } else {
+                console.log(stock + " data wasn't found in redis");
+                // otherwise attempt to get the data from finance server
+                getFromFinanceServer(stock, function (financeServerData) {
+                    // can the data be retrieved from finance server?
+                    if (financeServerData) {
+                        console.log(stock + " data found in back end server, writing to redis with expiration");
+                        // store it in cache
+                        redisClient.set(stock, financeServerData);
+
+                        // set it to expire in 30 seconds
+                        redisClient.expire(stock, 20)
+                        // write it to data
+                        data = financeServerData;
+
+                    } else {
+                        console.log(stock + " data cannot be retrieved from backend server");
+                        // if it cannot be retrieved from finance serfver retrurn server error
+                        res.statusCode = 500;
+                    }
+                });
             }
-
-            var stockData = "";
-            response.on('data', function (d) {
-                stockData += d;
-            });
-            response.on("end", function () {
-                res.end(stockData);
-            });
-            response.on("error", function () {
-                res.statusCode = 204;
-                res.end();
-            });
         });
     } else {
-        res.end();
+        // stock cannot be retrieved, return 'Not found'
+        res.statusCode = 400;
     }
+
+    res.end(data);
 });
 
-app.get('/checkdb', function (req, res) {
-    // Connection URL
-    var mongoPort = process.env.MISTRAL_MONGO_PORT;
-    if (mongoPort) {
-        mongoPort = mongoPort.substr(6);
-    } else {
-        mongoPort = "0.0.0.0:" + MONGO_PORT;
-    }
-    var url = "mongodb://unit_test_user:run@" + mongoPort;
-    mongoClient.connect(url, function (err, db) {
-        if (err) {
-            res.send(err.toString());
-        }
-        else {
-            res.send("ok");
-            return;
-        }
-        db.close();
-    });
-}
-)
+
 
 app.listen(PORT, function () {
-    console.log('App listening on port ' + PORT.toString() + '!')
+    appInsightsClient.trackEvent("AppStartListen");
+    console.log('Now listening on port ' + PORT.toString() + '!');
 })
